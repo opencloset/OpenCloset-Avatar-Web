ad2da7589accaf6399420c1d8a1a852193274947
Hyungsuk Hong <aanoaa@gmail.com>
2016-01-09 17:15:40 +0900

    Implements Avatar controller

--- lib/OpenCloset/Avatar/Web/Controller/Avatar.pm
+++ lib/OpenCloset/Avatar/Web/Controller/Avatar.pm
@@ -1,5 +1,14 @@
 package OpenCloset::Avatar::Web::Controller::Avatar;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojolicious::Types;
+
+use Digest::MD5 ();
+use HTTP::Tiny;
+use Image::Info ();
+use Image::Resize;
+use Path::Tiny ();
+
+has schema => sub { shift->app->schema };
 
 =head1 METHODS
 
@@ -32,11 +41,69 @@ sub md5sum {
     my $self   = shift;
     my $md5sum = $self->param('md5sum');
 
-    my $d = $self->param('d');
-    my $s = $self->param('s');
-    return $self->redirect_to($d) if $d;
+    my $v = $self->validation;
+    $v->optional('d');
+    $v->optional('s')->like(qr/^[0-9]+$/);
 
-    # http://www.example.com/default.jpg 의 md5sum 으로 찾아보고 없으면 fetch 해서 처리
+    return $self->error( 400, 'Failed to validation: ' . join( ', ', @{ $v->failed } ) ) if $v->has_error;
+
+    my $d = $v->param('d');
+    my $s = $v->param('s');
+
+    my $avatar = $self->schema->resultset('Avatar')->find( { md5sum => $md5sum } );
+    my $image;
+    if ($avatar) {
+        my ( $prefix, $rest ) = $md5sum =~ /(\w{2})(\w*)/;
+        $image = Path::Tiny::path( $self->app->home->rel_file("public/thumbnails/$prefix/$rest") );
+        $image->touchpath && $image->spew_raw( $avatar->image ) unless $image->exists;
+    }
+    else {
+        if ( $d && $d =~ /^http/ ) {
+            my $md5sum = Digest::MD5::md5_hex($d);
+            my ( $prefix, $rest ) = $md5sum =~ /(\w{2})(\w*)/;
+            $image = Path::Tiny::path( $self->app->home->rel_file("public/thumbnails/$prefix/$rest") );
+            unless ( $image->exists ) {
+                my $res = HTTP::Tiny->new( timeout => 1 )->get($d);
+                if ( $res->{success} && $res->{headers}{'content-type'} =~ /image/ ) {
+                    $image->touchpath;
+                    $image->spew_raw( $res->{content} );
+                }
+                else {
+                    $image = undef;
+                }
+            }
+        }
+    }
+
+    unless ($image) {
+        my $md5sum = Digest::MD5::md5_hex('default');
+        my ( $prefix, $rest ) = $md5sum =~ /(\w{2})(\w*)/;
+        my $avatar = $self->schema->resultset('Avatar')->find( { md5sum => $md5sum } );
+        $image = Path::Tiny::path( $self->app->home->rel_file("public/thumbnails/$prefix/$rest") );
+        $image->touchpath && $image->spew_raw( $avatar->image ) unless $image->exists;
+    }
+
+    if ($s) {
+        my $ir     = Image::Resize->new("$image");
+        my $gd     = $ir->resize( $s, $s );
+        my $resize = Path::Tiny::path( sprintf '%ss=%dx%d', $image, $s, $s );
+        $resize->spew_raw( $gd->png ) unless $resize->exists;
+        $image = $resize;
+    }
+
+    my $parent = $image->parent;
+    my $types  = Mojolicious::Types->new;
+    my $type   = Image::Info::image_type("$image");
+
+    return $self->error( 400, "Not supported image: $type->{error}" ) if $type->{error};
+
+    my $mime_type = $types->type( lc $type->{file_type} );
+
+    return $self->error( 400, "Not supported image type: $type->{file_type}" ) unless $mime_type;
+
+    $self->res->headers->content_type($mime_type);
+
+    return $self->reply->static( sprintf "thumbnails/%s/%s", $parent->basename, $image->basename );
 }
 
 =head2 create
@@ -45,6 +112,8 @@ sub md5sum {
     POST /avatar
 
 =over
+
+=item token
 
 =item key
 
@@ -56,6 +125,39 @@ sub md5sum {
 
 sub create {
     my $self = shift;
+
+    my $v = $self->validation;
+
+    my @tokens = ( $self->config->{token} );
+    $v->required('token')->in(@tokens);
+    $v->required('key');
+    $v->required('img');
+
+    return $self->error( 400, 'Failed to validation: ' . join( ', ', @{ $v->failed } ) ) if $v->has_error;
+
+    my $key = $v->param('key');
+    my $img = $v->param('img');
+
+    my $avatar = $self->schema->resultset('Avatar')
+        ->update_or_create( { md5sum => Digest::MD5::md5_hex($key), image => $img->slurp } );
+
+    return $self->error( 500, 'Failed to create avatar' ) unless $avatar;
+
+    ## image 가 등록/변경되고 나면 관련된 thumbnails 를 지운다
+    my ( $prefix, $rest ) = $avatar->md5sum =~ /(\w{2})(\w*)/;
+    my $dir = Path::Tiny::path( $self->app->home->rel_dir("public/thumbnails/$prefix") );
+    for my $file ( $dir->children ) {
+        $file->remove if $file->basename =~ /$rest/;
+    }
+
+    $self->render(
+        json => {
+            id          => $avatar->id,
+            md5sum      => $avatar->md5sum,
+            create_date => $avatar->create_date,
+            update_date => $avatar->update_date
+        }
+    );
 }
 
 1;
